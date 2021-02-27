0c5483a7e3f318b698461ceaf566464c9e7098be
Bagamolbaga <40855088+Bagamolbaga@users.noreply.github.com>
2021-02-27 21:42:45 +0300

    Сделаны кнопки ДОБАВИТЬ\ЗАКРЫТЬ

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -29,10 +29,59 @@ const Style_TodoInputContainer = styled.div`
   }
 `;
 
+const Style_TodoInputAddContainer = styled.div`
+  width: 100%;
+  height: 40px;
+  margin-top: 20px;
+  opacity: 0;
+  transition: all 0.5s ease-out;
+  ${Input}:focus + & {
+    opacity: 1;
+    transition: all 0.1s ease-out;
+  }
+`;
+
+const Style_TodoInputAddButton = styled.button`
+  width: 135px;
+  height: 34px;
+  text-align: center;
+  background: #4dd599;
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  color: #fff;
+  margin-right: 20px;
+  cursor: pointer;
+  &:active {
+    background: #2aac73;
+  }
+`;
+
+const Style_TodoInputCloseButton = styled(Style_TodoInputAddButton)`
+  background: #F4F6F8;
+  color: #9C9C9C;
+  &:active {
+    background: #7b7a7a;
+  }
+`;
+
+const Style_TodoInputI = styled.i`
+  color: #9C9C9C;
+  s
+`;
+
 const TodoInput = () => {
   return (
     <Style_TodoInputContainer>
+        <Style_TodoInputI>
+            <i class="fas fa-plus"></i>
+        </Style_TodoInputI>
+        
       <Input placeholder="New todo"></Input>
+      <Style_TodoInputAddContainer>
+        <Style_TodoInputAddButton>ADD</Style_TodoInputAddButton>
+        <Style_TodoInputCloseButton>CLOSE</Style_TodoInputCloseButton>
+      </Style_TodoInputAddContainer>
     </Style_TodoInputContainer>
   );
 };
